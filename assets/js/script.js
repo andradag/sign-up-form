1b12954af99d0e63fe6590e55de2ef0f5c85af41
Andrada Gacichevici <andradagacichevici@gmail.com>
2022-09-28 19:29:17 +0100

    add submit event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // target form
-const formElement = document.querySelector("#sign-up");
+const formElement = document.querySelector("#login-form");
 
 // target form inputs
 const emailInput = document.querySelector("#email");
@@ -11,3 +11,19 @@ const msgDiv = document.querySelector("#msg");
 // target the last registered user elements
 const userEmailSpan = document.querySelector("#user-email");
 const userPasswordSpan = document.querySelector("#user-password");
+
+// on submit event handler function
+const handleSubmit = function (event) {
+  event.preventDefault();
+
+  console.log("submit");
+
+  // get data from local storage
+
+  // update the data from local storage
+
+  // set local storage with updated data
+};
+
+// add event listener on form for submit event
+formElement.addEventListener("submit", handleSubmit);
