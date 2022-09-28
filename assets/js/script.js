f2e9101fe0a576f5ea7d2d7e285844fa312d2172
Andrada Gacichevici <andradagacichevici@gmail.com>
2022-09-28 19:11:24 +0100

    target form, form inputs,  feedback message

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// target form
+const formElement = document.querySelector("#sign-up");
+
+// target form inputs
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
+
+//  target the feedback message
+const msgDiv = document.querySelector("#msg");
+
+// target the last registered user elements
+const userEmailSpan = document.querySelector("#user-email");
+const userPasswordSpan = document.querySelector("#user-password");
