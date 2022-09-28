64caa4aff766f34fb2294684e63e501c7c5776aa
Andrada Gacichevici <andradagacichevici@gmail.com>
2022-09-28 20:21:05 +0100

    setting up error message

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,26 @@ const msgDiv = document.querySelector("#msg");
 const userEmailSpan = document.querySelector("#user-email");
 const userPasswordSpan = document.querySelector("#user-password");
 
+const renderLastRegisteredUser = function (lastRegisteredUser) {};
+
+const getDataFromLocalStorage = function () {
+  // get data from Local Storage by key name
+  const data = JSON.parse(localStorage.getItem("lastRegisteredUser"));
+
+  if (!data) {
+    // set local storage with default value {} return {}
+    localStorage.setItem("lastRegisteredUser", JSON.stringify({}));
+    return {};
+  } else {
+    // else return the parsed data from local storage
+    return data;
+  }
+};
+
+const renderError = function (errorMessage) {
+  msgDiv.textContent = errorMessage;
+};
+
 // on submit event handler function
 const handleSubmit = function (event) {
   event.preventDefault();
@@ -19,21 +39,44 @@ const handleSubmit = function (event) {
   console.log("submit");
 
   // get data from local storage
+  const lastRegisteredUser = getDataFromLocalStorage();
+  console.log(lastRegisteredUser);
+
+  // get the user input values
+  const email = emailInput.value;
+  const passwoord = passwordInput.value;
+
+  //validation
+  if (!email) {
+    // render error message
+    renderError("Please enter a valid email");
+    return;
+  }
+
+  if (!password) {
+    // render error message
+    renderError("Please enter a valid password");
+    return;
+  }
 
   // update the data from local storage
 
   // set local storage with updated data
 
   // render last registered user with local storage data
+  renderLastRegisteredUser(lastRegisteredUser);
 };
 
 // on load event handler function
 const handleLoad = function () {
-  console.log("load");
-
   // get data from local storage
+  const lastRegisteredUser = getDataFromLocalStorage();
 
-  // render last registered user with local storage data
+  // render only if oject is not empty
+  if (lastRegisteredUser.email && lastRegisteredUser.password) {
+    // render last registered user with local storage data
+    renderLastRegisteredUser(lastRegisteredUser);
+  }
 };
 
 // add event listener on form for submit event
